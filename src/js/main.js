1d4d83906ffb6509304ab45413b59d95120bb8af
Marina Moroz <moroz2387moroz@yandex.ru>
2023-10-21 08:44:08 +1000

    add slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,21 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
-
+window.addEventListener("DOMContentLoaded", () => {
+  const splide = new Splide(".splide", {
+    type: "loop",
+    pagination: false,
+    arrows: false,
+    autoplay: true,
+    perPage: 3,
+    focus: "center",
+    height: "660px",
+    fixedWidth: "990px",
+    gap: 30,
+    padding: "40%",
+    interval: 2000,
+    // breakpoints: {
+    //   640: {
+    //     height: "6rem",
+    //   },
+    // },
+  });
+  splide.mount();
+});
